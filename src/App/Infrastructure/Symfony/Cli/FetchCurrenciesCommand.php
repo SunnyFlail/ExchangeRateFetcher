a99d7dd1a33d1d2f67af7c1caace9f7e10b5e5b4
Patryk Ćwikliński <sunny.flail@gmail.com>
2024-04-18 16:45:49 +0200

    Added symfony command

---
+++ src/App/Infrastructure/Symfony/Cli/FetchCurrenciesCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SunnyFlail\ExchangeRateFetcher\App\Infrastructure\Symfony\Cli;
+
+use SunnyFlail\ExchangeRateFetcher\App\MessageBus\MessageBusInterface;
+use SunnyFlail\ExchangeRateFetcher\App\Message\FetchExchangeRates;
+use SunnyFlail\ExchangeRateFetcher\NBPApi\Enum\Table;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand('app:fetch-currency-rates')]
+final class FetchCurrenciesCommand extends Command
+{
+    public function __construct(private MessageBusInterface $messageBus)
+    {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $style = new SymfonyStyle($input, $output);
+
+        try {
+            $this->messageBus->dispatch(new FetchExchangeRates(
+                Table::A,
+                new \DateTimeImmutable()
+            ));
+        } catch (\Throwable $e) {
+            $style->error([
+                'An exception occured while fetching exchange rates:',
+                $e->getMessage(),
+            ]);
+
+            return Command::FAILURE;
+        }
+
+        $style->success('Successfully fetched exchange rates.');
+
+        return Command::SUCCESS;
+    }
+}
